79ffd737ab237501c6162d87e0405aabcac60df9
Ankit Patel <165417853+AnkitPatel00@users.noreply.github.com>
2024-12-16 18:15:28 +0530

    Update Header.jsx

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -46,7 +46,7 @@ const Header = () => {
       <nav className="navbar navbar-expand-lg navbar-light bg-body-tertiary">
         <div className="container-fluid">
           <Link className="navbar-brand" to="/">
-            <img src="../../../icons/clothstore.svg" alt="logo" width={50} />
+            <img src="../icons/clothstore.svg" alt="logo" width={50} />
           </Link>
           <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
